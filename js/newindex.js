a2423d56c8901ddbd686826793eead966d10b5e7
greetingcards <162377092+greetingcards@users.noreply.github.com>
2024-03-08 01:25:18 +0300

    Add files via upload

--- js/newindex.js
+++ js/newindex.js
@@ -30,7 +30,7 @@ canv.drawImage(photo,0,0);
 function DrawText(canv,fillcolor) {
     canv.fillStyle = fillcolor; 
     canv.textBaseline = 'middle';
-    canv.font = " 23px Aref Ruqaa";
+    canv.font = " 20px aktiv-grotesk-arabic ";
 }
 function DynamicText(canv,photo,clr,x,y) {
   document.getElementById('name').addEventListener('keyup', function() {
